b2b945599b475c1797511a01fac6eb570870127d
indra343 <90840871+indra343@users.noreply.github.com>
2021-09-24 16:19:47 +0530

    Create assignment2.java

---
+++ assignment2.java
@@ -0,0 +1,20 @@
+// assignment 2 set 1
+class one{
+    float s=7.5f;
+    
+}
+public class two extends one{
+    float b=2.3f;
+    public static void main(String args[]){
+        two o=new two();
+        
+        System.out.println(o.s+o.b);
+        
+    }
+}
+output
+9.8
+
+
+
+
